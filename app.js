cd7fce32ed494fd7a828d3a39e103ca9e14189ee
Pilo2201 <italoarata99@gmail.com>
2025-03-18 00:27:44 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Varibales
+let amigos = [];
+
+//Funciones
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if (amigo !== '') {
+        amigos.push(amigo);
+        document.getElementById('amigo').value = '';
+        console.log(amigos);
+        actualizarLista();
+    } else {
+        alert('Por favor, inserte un nombre.');
+    }
+}
+
+//Función para actualizar la lista de amigos
+function actualizarLista() {
+    // Obtener el elemento de la lista
+    let lista = document.getElementById('listaAmigos');
+
+    // Limpia la lista
+    lista.innerHTML = '';
+
+    // Itera sobre el arreglo 'amigos'
+    for (let i = 0; i < amigos.length; i++) {
+        // Crea un nuevo elemento de lista <li>
+        let li = document.createElement('li');
+
+        // Asigna el nombre del amigo al texto del elemento de lista
+        li.innerHTML = amigos[i];
+
+        // Agrega el nuevo elemento a la lista en el DOM
+        lista.appendChild(li);
+    }
+}
+
+    function sortearAmigo() {
+        if (amigos.length !== 0) {
+            // Generar un índice aleatorio
+            let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+            // Obtener el nombre sorteado
+            let amigoSeleccionado = amigos[indiceAleatorio];
+
+            // Mostrar el resultado en la página
+            document.getElementById('resultado').innerHTML = amigoSeleccionado;
+        } else {
+            alert('No hay amigos disponibles para seleccionar, por favor inserte un nombre.');
+        }
+    }
